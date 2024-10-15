3f204cabf4469b9ba8a9f078ff2e4c30b28d6bf1
GL4CE <54809415+GL4CE@users.noreply.github.com>
2024-10-15 22:00:18 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The exclamation Tree",
+	id: "exc",
+	author: "exc!",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
